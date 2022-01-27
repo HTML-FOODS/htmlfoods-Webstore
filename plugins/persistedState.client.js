948ed05813823e3847e48832580be30f2f80e0b7
mydarVGG <hameedah.adebanjo@venturegardengroup.com>
2022-01-27 10:07:08 +0100

    generated

--- plugins/persistedState.client.js
+++ plugins/persistedState.client.js
@@ -2,6 +2,6 @@ import createPersistedState from "vuex-persistedstate";
 
 export default ({ store }) => {
   createPersistedState({
-    token: store.state.admin.token
+    cart: store.state.cart.cart
   })(store);
 };
